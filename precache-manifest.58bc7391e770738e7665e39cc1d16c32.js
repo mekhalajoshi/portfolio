765245c267cf484df52846f5a1bdb780c4c7e446
Mekhala Joshi <mekhalajoshi@gmail.com>
2020-01-07 15:57:07 -0500

    Updates

---
+++ precache-manifest.58bc7391e770738e7665e39cc1d16c32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ed3366bee7e785c72ae9f8704a7345",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "5c7b94131fb67ba1c284",
+    "url": "/Portfolio/static/css/main.6b09c966.chunk.css"
+  },
+  {
+    "revision": "92552cd12e599f64f0b8",
+    "url": "/Portfolio/static/js/2.dbfd339a.chunk.js"
+  },
+  {
+    "revision": "103611f4674bc0034f69645c7a543684",
+    "url": "/Portfolio/static/js/2.dbfd339a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5c7b94131fb67ba1c284",
+    "url": "/Portfolio/static/js/main.3ca4b57d.chunk.js"
+  },
+  {
+    "revision": "89ac0b145cc4677f0d5a",
+    "url": "/Portfolio/static/js/runtime-main.c29e4513.js"
+  }
+]);
