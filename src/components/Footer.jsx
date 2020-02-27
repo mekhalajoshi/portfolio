15b3b4e1515ee71f898bfda2674652b29a542cee
Mekhala Joshi <mekhalajoshi@users.noreply.github.com>
2020-02-27 13:59:05 -0500

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import KeyboardArrowUpOutlinedIcon from '@material-ui/icons/KeyboardArrowUpOutlined';
+import KeyboardArrowUpOutlinedIcon from '@material-ui/icons/KeyboardArrowUpOutlined'
 
 import '../css/components/Footer.css'
 
 export default function Footer(props) {
-  return (
-    <div className='footer_container'>
-      <div className="back_to_top_button_container" >
-        <button className='back_to_top_button' onClick={() => props.onClick('landingPage')} aria-label="edit">
-          <KeyboardArrowUpOutlinedIcon />
-        </button>
-      </div>
-      <div className='footer'>
-      </div>
-    </div>
-  )
+	return (
+		<div className='footer_container'>
+			<div className="back_to_top_button_container" >
+				<button className='back_to_top_button' onClick={() => props.onClick('landingPage')} aria-label="edit">
+					<KeyboardArrowUpOutlinedIcon />
+				</button>
+			</div>
+			<div className='footer'>
+			</div>
+		</div>
+	)
 }
